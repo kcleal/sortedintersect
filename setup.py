97feacaa5c4cf4946d3b2efc305bfb86995f94bd
kcleal <clealk@cardiff.ac.uk>
2024-04-26 16:46:00 +0100

    Updated build

--- setup.py
+++ setup.py
@@ -1,21 +1,19 @@
-from setuptools.extension import Extension
-from setuptools import setup, find_packages
+from setuptools import setup, find_packages, Extension
 
-ext_modules = list()
+ext_modules = [
+    Extension("sortedintersect.sintersect",
+              ["sortedintersect/sintersect.pyx"],
+              language="c++",
+              extra_compile_args=["-std=c++17"])
+]
 
-ext_modules.append(Extension("sortedintersect.sintersect",
-                             ["sortedintersect/sintersect.pyx"],
-                             language="c++",
-                             extra_compile_args=["-std=c++17"]
-                             ))
-
-setup(version='0.4.2',
-      name='sortedintersect',
-      description="Interval intersection for sorted query and target intervals",
-      author="Kez Cleal",
-      author_email="clealk@cardiff.ac.uk",
-      packages=find_packages(),
-      setup_requires=['cython'],
-      install_requires=['cython'],
-      ext_modules=ext_modules,
+setup(
+    version='0.4.2',
+    name='sortedintersect',
+    description="Interval intersection for sorted query and target intervals",
+    author="Kez Cleal",
+    author_email="clealk@cardiff.ac.uk",
+    packages=find_packages(),
+    install_requires=['Cython'],  # Ensure runtime dependencies are listed if any
+    ext_modules=ext_modules,
 )
